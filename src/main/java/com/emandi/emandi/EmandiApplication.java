7277028f22e254b71af4b7be380f5d8a5f7e55d9
Neha <mausuminanda977@gmail.com>
2025-03-26 14:00:46 +0530

    Initial commit

---
+++ src/main/java/com/emandi/emandi/EmandiApplication.java
@@ -0,0 +1,14 @@
+package com.emandi.emandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmandiApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(EmandiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emandi/emandi/EmandiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emandi.emandi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmandiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
